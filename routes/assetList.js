f1c5a5208f577fc97e2fb8c24b14442ea7f9b3f8
ryuken73 <ryuken73@gmail.com>
2022-10-02 12:03:19 +0900

    update: add default db.json and reset db.json router

--- routes/assetList.js
+++ routes/assetList.js
@@ -7,4 +7,10 @@ router.get('/', async (req, res, next) => {
 	res.json({success:true, assetList: assets||[]});
 })
 
+router.put('/default', async (req, res, next) => {
+	const assets = await global.db.selectAssets()
+	console.log(assets)
+	res.json({success:true, assetList: assets||[]});
+})
+
 module.exports = router;
